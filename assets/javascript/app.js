f414cad1c0b4b3b27d8e7e9636f6b7c6d5c2915c
VivaMango <VivaMangoMail@gmail.com>
2018-12-28 17:30:50 -0500

    AJAX to GIPHY API working. Chrome CORB error.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,34 @@
 console.log("API Test") //TESTING HTML/JS LINK !!SUCCESS!!
+
+// Setting up our variables for Ajax
+var APIKey = "YcQglRE4TS4xD2BTKk106HhEgiRLbsym" //VivaMango's GIPHY API Key
+var searchTerm //defining to use later in AJAX on-click
+var queryURL //defining to use later in response
+
+
+
+// Getting the input value from our giphyInput and preventing default action of giphySubmit button
+$("#giphySubmit").on("click", function() {
+    event.preventDefault()
+    console.log("Button on-click Test") //WORKING
+    var searchTerm = $("#giphyInput").val() //saving input field value as a variable
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + APIKey //structuring our queryURL based on GIPHY documentation
+    console.log (searchTerm , "searchTerm .val test") //WORKING
+    // AJAX CALL TO GIPHY API
+    $.ajax({
+        url: queryURL, //using queryURL structured variable
+        method: "GET" //GET method for getting GIFs back as response JSON
+    }).then(function(response) {  //function utilizing the JSON response object from GIPHY
+        console.log(response); //WORKING
+        console.log(response.data , "response.data test") //WORKING
+        console.log(response.data[0] , "response.data0 test") //WORKING
+        console.log(response.data[0].embed_url , "embed_url test") //WORKING
+        var embed_urltest = response.data[1].embed_url //saving access to JSON as variable
+        var newGIF = $("<img>") //Making a new HTML image container for our GIF
+        newGIF.attr("src" , embed_urltest) //adding the SRC attribute link to our GIF
+        console.log(response.data[1] , "responsedata1") //WORKING
+        $("#giphyDisplay").append(newGIF) //***********CROSS-ORIGIN READ BLOCKING ERROR HAPPENS HERE*************
+    });
+});
+
+
